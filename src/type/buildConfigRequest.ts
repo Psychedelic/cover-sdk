e943d9c6c12ce5a2360625c39e7fde3f4196ddd5
Scott Dn <scott.dn91@gmail.com>
2022-10-25 18:26:25 +0700

    feat: concrete type and docs

---
+++ src/type/buildConfigRequest.ts
@@ -0,0 +1,4 @@
+import {AnonymousBuildRequest, BuildRequest} from './buildRequest';
+
+export type BuildConfigRequest = BuildRequest;
+export type AnonymousBuildConfigRequest = AnonymousBuildRequest;
--- src/type/index.ts
+++ src/type/index.ts
@@ -1,3 +1,5 @@
 export * from './buildRequest';
+export * from './buildConfigRequest';
 export * from './buildWithConfigRequest';
+export * from './coverMetadataRequest';
 export * from './errorResponse';
